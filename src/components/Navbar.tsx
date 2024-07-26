980276768d51a104133cbc166b32058deae7b0c1
apheecan <emccjob@gmail.com>
2024-07-26 11:52:04 -0500

    add header flex

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,19 +4,21 @@ type NavbarProps = {
 
 const Navbar = ({ name }: NavbarProps) => {
   return (
-    <div className="w-full grid grid-cols-8 pt-6">
-      <div className="col-start-2 col-span-6">
-        <section className="flex sticky font-mono justify-between w-full items-center">
-          <h1 className="sm:text-sm md:text-3xl font-bold">{name}</h1>
+    <header className="flex justify-center items-center">
+      <div className="w-full max-w-[1440px] grid grid-cols-8">
+        <div className="col-start-2 col-span-6 pt-6">
+          <section className="flex sticky font-mono justify-between w-full items-center">
+            <h1 className="sm:text-sm md:text-3xl font-bold">{name}</h1>
 
-          <div className="flex justify-around w-48">
-            <a href="">CONTACT</a>
-            <a href="">ABOUT</a>
-          </div>
-        </section>
-        <hr className="w-5/6 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700" />
+            <div className="flex justify-around w-48 sm:text-sm">
+              <a href="">CONTACT</a>
+              <a href="">ABOUT</a>
+            </div>
+          </section>
+          <hr className="w-5/6 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700" />
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
 
