344d591e5cccc8bb93ad3e08ddfefe4406a94611
apheecan <emccjob@gmail.com>
2024-07-30 12:47:48 -0500

    Load photos from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import './App.css'
-import { city, forest, mountain, woman } from './assets/images/index'
 import PhotoGallery from './components/PhotoGallery'
 import DefaultLayout from './layouts/default'
-
-const photoSource = [city, woman, mountain, forest, forest, mountain, woman, city]
 
 function App() {
   return (
     <>
       <DefaultLayout>
-        <PhotoGallery photoSource={photoSource} />
+        <PhotoGallery />
       </DefaultLayout>
     </>
   )
--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -1,16 +1,41 @@
+import { useEffect, useState } from 'react'
+
+import { Photo } from '@src/types/photo'
+
 import PhotoCard from './PhotoCard'
 
-export type PhotoSourceList = {
-  photoSource: string[]
-}
+const PHOTO_API_URL = import.meta.env.VITE_PHOTO_API_URL
 
-const PhotoGallery = ({ photoSource }: PhotoSourceList) => {
-  const photos = photoSource.slice()
+const PhotoGallery = () => {
+  const [photos, setPhotos] = useState<Photo[] | []>([])
+  // const [loading, setLoading] = useState(false)
+  const getPhoto = async () => {
+    try {
+      const response = await fetch(PHOTO_API_URL, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      if (response.ok) {
+        const data: Photo[] = await response.json()
+        setPhotos(data)
+      }
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
+  }
+  useEffect(() => {
+    getPhoto()
+  }, [])
+  console.log(photos)
 
   return (
     <div className='grid gap-4 grid-cols-4'>
       {photos.map((photo) => (
-        <PhotoCard source={photo} />
+        <PhotoCard {...photo} />
       ))}
     </div>
   )
