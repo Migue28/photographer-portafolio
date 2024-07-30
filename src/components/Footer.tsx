d24de313fd02db8f43504095d79520749a89df48
Eddy Cedeño <miguepoke28@gmail.com>
2024-07-29 21:50:08 -0500

    Add name variable

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,13 @@
-const Footer = () => {
+type FooterProps = {
+  name: string
+}
+
+const Footer = ({ name }: FooterProps) => {
   const currentYear = new Date().getFullYear()
   return (
     <footer className='flex w-screen bg-zinc-300 justify-center items-end'>
       <p className='text-center font-mono font-light tracking-wide text-neutral-700'>
-        SKYLA RANGER {currentYear}
+        {name} {currentYear}
       </p>
     </footer>
   )
--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -4,11 +4,12 @@ import Footer from '@src/components/Footer'
 import Navbar from '@src/components/Navbar'
 
 const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
+  const name = 'SKYLA RANGER'
   return (
     <div className='flex flex-col h-screen'>
-      <Navbar name='SKYLA RANGER' />
+      <Navbar name={name} />
       <main>{children}</main>
-      <Footer />
+      <Footer name={name} />
     </div>
   )
 }
