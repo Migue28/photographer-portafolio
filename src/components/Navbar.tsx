94c13429450da06e7b7ac507f33e8741b822f9aa
Eddy Cedeño <miguepoke28@gmail.com>
2024-07-30 21:58:19 -0500

    fix paddings and margins

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = ({ name }: NavbarProps) => {
   return (
     <header className='flex justify-center items-center sticky top-0 z-50 bg-white'>
       <div className='w-full max-w-[1440px] grid grid-cols-8'>
-        <div className='col-start-2 col-span-6 pt-6'>
+        <div className='col-start-2 col-span-6 pt-1'>
           <section className='flex sticky font-mono justify-between w-full items-center'>
             <h1 className='sm:text-sm md:text-3xl font-bold'>{name}</h1>
 
@@ -15,7 +15,7 @@ const Navbar = ({ name }: NavbarProps) => {
               <a href=''>ABOUT</a>
             </div>
           </section>
-          <hr className='w-5/6 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:mb-10 dark:bg-gray-700' />
+          <hr className='w-5/6 h-1 mx-auto my-2 bg-gray-100 border-0 rounded md:mb-2 dark:bg-gray-700' />
         </div>
       </div>
     </header>
