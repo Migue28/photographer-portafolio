f2a891d63320c81375ac04e1c26d167b23bc0aa3
Eddy Cedeño <miguepoke28@gmail.com>
2024-07-31 10:31:55 -0500

    fixing loading animation

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -26,18 +26,33 @@ const PhotoGallery = () => {
     } catch (error) {
       console.error(error)
       throw error
-    } finally {
+    }
+    if (photos) {
       setLoading(false)
     }
   }
   useEffect(() => {
     getPhoto()
-  }, [])
+  })
 
   return (
     <div className='grid gap-4 grid-cols-4'>
-      {loading ? (
-        <div className='h-5 w-5 animate-spin rounded-full border-b-2 border-zinc-500s' />
+      {loading && !photos ? (
+        <div className='border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto'>
+          <div className='animate-pulse flex space-x-4'>
+            <div className='rounded-full bg-slate-700 h-10 w-10'></div>
+            <div className='flex-1 space-y-6 py-1'>
+              <div className='h-2 bg-slate-700 rounded'></div>
+              <div className='space-y-3'>
+                <div className='grid grid-cols-3 gap-4'>
+                  <div className='h-2 bg-slate-700 rounded col-span-2'></div>
+                  <div className='h-2 bg-slate-700 rounded col-span-1'></div>
+                </div>
+                <div className='h-2 bg-slate-700 rounded'></div>
+              </div>
+            </div>
+          </div>
+        </div>
       ) : (
         photos.map((photo) => <PhotoCard {...photo} />)
       )}
