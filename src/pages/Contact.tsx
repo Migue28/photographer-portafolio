721e3f0e546ad2e1a6cebeb3c19e4db79f9143ae
Eddy Cedeño <miguepoke28@gmail.com>
2024-08-02 20:20:51 -0500

    Add bg with image

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,11 @@
 const Contact = () => {
-  return <></>
+  return (
+    <div className='flex flex-col justify-center items-center'>
+      <h1 className='text-6xl mb-4'>Want to create together?</h1>
+      <div className='object-none w-full h-screen bg-mountain bg-opacity-50'>
+        <form action=''></form>
+      </div>
+    </div>
+  )
 }
 export default Contact
